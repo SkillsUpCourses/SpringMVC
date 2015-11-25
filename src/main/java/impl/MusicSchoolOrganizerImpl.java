46b4b263baff279cc660e69fcbec542ed9588821
Roman_Mosiienko <roman.mosiienko@gmail.com>
2015-11-25 09:50:58 +0200

    Init commit

---
+++ src/main/java/impl/MusicSchoolOrganizerImpl.java
@@ -0,0 +1,16 @@
+package impl;
+
+import api.MusicSchoolOrganizer;
+
+/**
+ * Class {@link MusicSchoolOrganizerImpl
+ *
+ * @author RomanMosiienko
+ * @version 1.0
+ * @since 24.11.15
+ */
+public class MusicSchoolOrganizerImpl implements MusicSchoolOrganizer {
+    public void organize(String test) {
+
+    }
+}
